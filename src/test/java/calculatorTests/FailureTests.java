7318bb53b5f8fe5023f781e494094f1d9ca2559b
Emisar <33164788+Emisar@users.noreply.github.com>
2020-03-26 01:16:52 +0400

    Add tests

---
+++ src/test/java/calculatorTests/FailureTests.java
@@ -0,0 +1,50 @@
+package calculatorTests;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import services.CountingService;
+
+public class FailureTests {
+    private static CountingService calculator;
+
+    @BeforeClass
+    public static void init() {
+        calculator = new CountingService();
+    }
+
+    @Test
+    public void test_add_2_plus_2() throws Exception {
+        String input = "2+3";
+        String res = calculator.solveEquation(input);
+        Assert.assertEquals("4", res);
+    }
+
+    @Test
+    public void test_sub_4_minus_2() throws Exception {
+        String input = "4-4";
+        String res = calculator.solveEquation(input);
+        Assert.assertEquals("2", res);
+    }
+
+    @Test
+    public void test_mult_2_and_2() throws Exception {
+        String input = "2*3";
+        String res = calculator.solveEquation(input);
+        Assert.assertEquals("4", res);
+    }
+
+    @Test
+    public void test_div_4_and_2() throws Exception {
+        String input = "3/2";
+        String res = calculator.solveEquation(input);
+        Assert.assertEquals("2", res);
+    }
+
+    @Test
+    public void test_equate() throws Exception {
+        String input = "(46 + 2) * ((357 - 60) / 10)";
+        String res = calculator.solveEquation(input);
+        Assert.assertEquals("1397", res);
+    }
+}
